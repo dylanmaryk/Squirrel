2853b53d1fd8771edcc34c0282ddb03a36a99c84
Dylan Maryk <dylan@dylanmaryk.com>
2013-12-05 17:07:58 +0000

    Change measurements.

---
+++ Squirrel/src/Detect.java
@@ -0,0 +1,67 @@
+import lejos.nxt.Motor;
+import lejos.robotics.subsumption.Behavior;
+
+public class Detect implements Behavior {
+	public boolean takeControl() { // Set locatedBall to false near end of action method
+		// If sees something nearby
+		if (Squirrel.us.getDistance() <= 40 && Squirrel.notDetected)
+			return true;
+		else
+			return false;
+	}
+	
+	public void action() {
+		Squirrel.suppressed = false;
+		Squirrel.tachoRotationA = Motor.A.getTachoCount();
+		Squirrel.tachoRotationB = Motor.B.getTachoCount();
+		// Squirrel.angleRotated = Squirrel.pilot.getAngleIncrement();
+		Squirrel.tachoDistance = 0;
+		Squirrel.notDetected = false;
+		
+		Motor.A.resetTachoCount();
+		Motor.B.resetTachoCount();
+		
+		Squirrel.pilot.travel(Squirrel.middleDistanceStep, true);
+		
+		while (Motor.A.isMoving()) {
+			// If sees something close
+			if (Squirrel.us.getDistance() <= 6) {
+				// Stop moving
+				Squirrel.pilot.stop();
+				
+				// Close gripper
+				Motor.C.rotate(-Squirrel.rotateAmount);
+				
+				// Wait 3 seconds
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				// If ball is not red
+				if (Squirrel.ls.readValue() < 46) {
+					// Let go of ball
+					Motor.C.rotate(Squirrel.rotateAmount);
+					
+					Squirrel.hasBall = false;
+					Squirrel.returnToExplore = true;
+				} else {
+					Squirrel.hasBall = true;
+					Squirrel.returnToExplore = false;
+					Squirrel.returnHome = true;
+				}
+				
+				break;
+			} else
+				Squirrel.returnToExplore = true;
+		}
+		
+		Squirrel.tachoDistance = Motor.A.getTachoCount();
+		// Squirrel.distanceMoved = Squirrel.pilot.getMovementIncrement();
+	}
+	
+	public void suppress() {
+		
+	}
+}
---
+++ Squirrel/src/Squirrel.java
@@ -0,0 +1,51 @@
+import lejos.nxt.LightSensor;
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.subsumption.Arbitrator;
+import lejos.robotics.subsumption.Behavior;
+
+public class Squirrel {
+	public static DifferentialPilot pilot = new DifferentialPilot(56, 110, Motor.A, Motor.B);
+	public static UltrasonicSensor us = new UltrasonicSensor(SensorPort.S2);
+	public static LightSensor ls = new LightSensor(SensorPort.S1);
+	public static int middleDistanceTotal = 0;
+	public static int rotateAmount = 110;
+	public static int middleDistanceStep = 200;
+	public static int distanceToTravel = 20;
+	public static int distanceToGrab = 10;
+	public static int tachoDistance = 0;
+	public static int tachoRotationA = 0;
+	public static int tachoRotationB = 0;
+	// public static float distanceMoved = 0;
+	// public static float angleRotated = 0;
+	public static boolean turnedLeft = false;
+	public static boolean suppressed = false;
+	public static boolean notDetected = true;
+	public static boolean hasBall = false;
+	public static boolean returnToExplore = false;
+	public static boolean returnHome = false;
+	
+	public static void main(String[] args) {
+		// Set wheel speeds
+		pilot.setTravelSpeed(100);
+		pilot.setRotateSpeed(100);
+		
+		// Set gripper speed
+		Motor.C.setSpeed(720);
+		
+		// Open gripper
+		Motor.C.rotate(rotateAmount);
+		
+		// Set up behaviours and arbitrator
+		Behavior behaviorExplore = new Explore();
+		Behavior behaviorDetect = new Detect();
+		Behavior behaviorReturn = new Return();
+		
+		Behavior[] behaviors = { behaviorExplore, behaviorDetect, behaviorReturn };
+		
+		Arbitrator ab = new Arbitrator(behaviors);
+		ab.start();
+	}
+}
